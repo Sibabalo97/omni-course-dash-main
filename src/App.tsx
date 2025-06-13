eb48af3d55972f14b271778f617d5e5a7d12adad
Sibabalo Maqiyane <Sibabalo.Maqiyane@redpandasoftware.co.za>
2025-06-13 13:22:43 +0200

    reverted some commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
+
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -20,7 +21,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/omni-course-dash-main">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/course/:id" element={<CourseDetail />} />
