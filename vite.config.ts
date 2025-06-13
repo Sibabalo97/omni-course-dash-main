a1cd4de86aa50ea699be9a09a7741fbe6f0a17dc
Sibabalo Maqiyane <Sibabalo.Maqiyane@redpandasoftware.co.za>
2025-06-13 12:09:27 +0200

    created yml file for deployment using CI/CD  and installed some depedencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import { defineConfig } from "vite";
 import path from "path";
 
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/omni-course-dash-main/", // Set the base path
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
-  
+ 
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "this": path.resolve(__dirname, "./src"), 
     },
   },
 }));
